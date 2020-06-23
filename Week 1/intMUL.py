15ed83153eb174c6bab995b7b615acac8b9415b3
anushkrishnav <anush.venkatakrishna@gmail.com>
2020-06-23 19:08:15 +0530

    MergeSort complete

--- Week 1/intMUL.py
+++ Week 1/intMUL.py
@@ -1,6 +1,32 @@
 import time
-x=int("\nenter x value :\t ")
-y=int("\n enter y value:\t ")
-k=len(str(x))
-j=len(str(y))
-def mul(x,y,k,j):
+def mergesort(values):
+    if len(values)>1: 
+        m = len(values)//2
+        left = values[:m] 
+        right = values[m:] 
+        left = mergesort(left) 
+        right = mergesort(right) 
+  
+        values =[] 
+  
+        while len(left)>0 and len(right)>0: 
+            if left[0]<right[0]:
+                values.append(left[0])
+                left.pop(0) 
+            else: 
+                values.append(right[0])
+                right.pop(0) 
+  
+        for i in left: 
+            values.append(i)
+        for i in right: 
+            values.append(i) 
+    return values 
+
+def main():
+    list1=[5,4,1,8,7,2,6,3]
+    print('unsorted set \n',list1)
+    print('sorted set \n ',mergesort(list1))
+
+if __name__ == "__main__":
+    main()
